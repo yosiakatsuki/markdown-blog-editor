94c1f793f4c17a1c9f3a73cb6f6a40092132e8f7
yosiakatsuki <y.ogata@yosiakatsuki.net>
2018-04-27 03:09:05 +0900

    release

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Markdown Blog Editor
-Markdownでブログ書きたいオレオレブログエディタ
+
+Markdownでブログ書きたい俺の、オレオレブログエディタ
+
+## こんな人におすすめ
+
+Markdownで書いて  
+→ HTMLに変換して  
+→ ブログの投稿画面に貼り付けて  
+→ 公開！！
+
+…な人におすすめ
+
+## Markdown→HTMLのオレオレ変換
+
+- `h`タグにつくid属性の削除
+	- 日本語の見出しの場合、idが`id="-"`のようになってしまい全然idじゃなくなるため
+- `h`タグの上下に空行1つ
+- `<!--more-->`タグの上下に空行1つ
+- `<pre>`タグの前後に空行1つ
+
+## 利用ライブラリ
+
+- [SimpleMDE - Markdown Editor](https://github.com/sparksuite/simplemde-markdown-editor)
+	- Markdownエディタ部分
+- [Marked](https://github.com/markedjs/marked)
+	- Markdown → HTML変換
+- [Bootstrap 4](https://github.com/twbs/bootstrap)
+- [Highlight.js](https://github.com/isagalaev/highlight.js)
+- [github-markdown-css](https://github.com/sindresorhus/github-markdown-css)
+- [jQuery](https://github.com/jquery/jquery)
---
+++ js/script.js
@@ -0,0 +1,74 @@
+document.addEventListener( 'DOMContentLoaded', function( event ) {
+  marked.setOptions({
+      breaks : true
+  })
+  var simplemde = new SimpleMDE({
+    element: document.getElementById('editor-main'),
+    autosave: {
+      enabled: true,
+      uniqueId: "markdownblogeditor",
+      delay: 1000,
+    },
+    forceSync: true,
+    spellChecker: false,
+    toolbar: false,
+    status: false
+  })
+  
+  if ( document.getElementById('editor-main').value ) {
+    previewMarked( document.getElementById('editor-main').value )
+  }
+  $('.CodeMirror').keyup(function() {
+    var text = $('#editor-main').val()
+    previewMarked( text )
+  })
+  document.getElementById('html-copy').addEventListener('click', copyhtml, false)
+});
+
+
+function previewMarked( text ) {
+  $('#preview').html( convertMarkdown( text ) )
+  $('#preview pre code').each(function(i, block) {
+      hljs.highlightBlock(block)
+  });
+}
+
+function copyhtml() {
+  var t = document.createElement('textarea')
+  var input = document.getElementById('editor-main').value
+  t.textContent = convertMarkdown( input )
+  var b = document.getElementsByTagName('body')[0]
+  b.appendChild(t)
+  t.select()
+  var copy = document.execCommand('copy')
+  b.removeChild(t)
+  return copy
+}
+
+
+function convertMarkdown( text ) {
+  text = marked(text)
+  /**
+   * オレオレ変換をかける
+   */
+  return text
+    .replace(/ id=".+?"/g,'')
+    .replace(/<p><p>/g,'<p>')
+    .replace(/<\/p><\/p>/g,'</p>')
+    .replace(/<h1>/g,"\n<h1>")
+    .replace(/<\/h1>/g,"</h1>\n")
+    .replace(/<h2>/g,"\n<h2>")
+    .replace(/<\/h2>/g,"</h2>\n")
+    .replace(/<h3>/g,"\n<h3>")
+    .replace(/<\/h3>/g,"</h3>\n")
+    .replace(/<h4>/g,"\n<h4>")
+    .replace(/<\/h4>/g,"</h4>\n")
+    .replace(/<h5>/g,"\n<h5>")
+    .replace(/<\/h5>/g,"</h5>\n")
+    .replace(/<h6>/g,"\n<h6>")
+    .replace(/<\/h6>/g,"</h6>\n")
+    .replace('<!--more-->',"\n<!--more-->\n")
+    .replace(/<pre>/g,"\n\n<pre>")
+    .replace(/<\/pre>/g,"</pre>\n\n")
+}
+
